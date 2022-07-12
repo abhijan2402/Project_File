53ba0325a43cd9ef82220c830117f75f890382cb
abhijan2402 <abhishek.jangid643@gmail.com>
2022-07-12 14:14:29 +0530

    making grp creation dynamic

--- src/Screens/Groups.js
+++ src/Screens/Groups.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -11,65 +11,70 @@ import {
   Image,
   ImageBackground
 } from 'react-native';
-import { API, Auth , Storage } from 'aws-amplify';
+import { API, Auth, Storage } from 'aws-amplify';
 import 'react-native-gesture-handler';
 import * as queries from '../graphql/queries';
 import Style from '../Styles/Style'
 import LinearGradient from 'react-native-linear-gradient';
 import GroupList from '../Styles/GroupList';
-const data=[
-  {GroupName:"Group 1",grpDescription:"hello"},
-  {GroupName:"Group 2",grpDescription:"hello"},
-  {GroupName:"Group 3",grpDescription:"hello"},
-  {GroupName:"Group 4",grpDescription:"hello"},
-  {GroupName:"Group 5",grpDescription:"hello"},
-  {GroupName:"Group 6",grpDescription:"hello"},
-  {GroupName:"Group 7",grpDescription:"hello"},
-  {GroupName:"Group 8",grpDescription:"hello"},
-  {GroupName:"Group 9",grpDescription:"hello"},
-  {GroupName:"Group 10",grpDescription:"hello"},
-  {GroupName:"Group 11",grpDescription:"hello"},
-  {GroupName:"Group 12",grpDescription:"hello"},
-  {GroupName:"Group 13",grpDescription:"hello"},
+const data = [
+  { GroupName: "Group 1", grpDescription: "hello this is jdsiud" },
+  { GroupName: "Group 2", grpDescription: "oadsshdbskh" },
+  { GroupName: "Group 3", grpDescription: "didkewkh" },
+  { GroupName: "Group 4", grpDescription: "hello" },
+  { GroupName: "Group 5", grpDescription: "hello" },
+  { GroupName: "Group 6", grpDescription: "hello" },
+  { GroupName: "Group 7", grpDescription: "hello" },
+  { GroupName: "Group 8", grpDescription: "hello" },
+  { GroupName: "Group 9", grpDescription: "hello" },
+  { GroupName: "Group 10", grpDescription: "hello" },
+  { GroupName: "Group 11", grpDescription: "hello" },
+  { GroupName: "Group 12", grpDescription: "hello" },
+  { GroupName: "Group 13", grpDescription: "hello" },
 ]
-const Groups=({navigation})=>{
-  const [groupList,setGroupList]=useState([]);
+const Groups = ({ navigation }) => {
+  const [groupList, setGroupList] = useState([]);
   const renderItem = ({ item }) => (
     <View style={GroupList.card} >
-      <TouchableOpacity 
-       onPress={()=>navigation.navigate("Groupchat",{groupName:item.GroupName,groupImage:item.GroupImageUrl})}
-        style={GroupList.groupContaier} 
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Groupchat", { groupName: item.GroupName, groupImage: item.GroupImageUrl })}
+        style={GroupList.groupContaier}
       >
         <View style={GroupList.cardHeader}>
-          <Image 
+          <View style={GroupList.crr}>
+
+            <Image
               style={GroupList.image}
-              source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7imXJMeK9b42t8JBjKxYP2zJd8MVrbVNEAg&usqp=CAU"}} 
-          />
+              source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7imXJMeK9b42t8JBjKxYP2zJd8MVrbVNEAg&usqp=CAU" }}
+            />
+          </View>
           <Text style={GroupList.grpName}>{item.GroupName}</Text>
         </View>
         <LinearGradient colors={['#BAF3FF', '#F0FCFF', '#BAF3FF']} style={GroupList.cardFooter}>
-          <Text style={GroupList.footerTxt}>Description....</Text>
+          <View >
+            <Text style={GroupList.footerTxt}>{item.grpDescription}</Text>
+          </View>
         </LinearGradient >
       </TouchableOpacity>
     </View>
   );
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     // fetchUserGroups();
-  },[groupList])
-  const fetchUserGroups=async()=>{
+  }, [groupList])
+  const fetchUserGroups = async () => {
     try {
-      const authedUser=await Auth.currentAuthenticatedUser();
-      const userGroups=await API.graphql({
-        query:queries.groupByUserName,
-        variables:{userID:authedUser.attributes.email}
+      const authedUser = await Auth.currentAuthenticatedUser();
+      const userGroups = await API.graphql({
+        query: queries.groupByUserName,
+        variables: { userID: authedUser.attributes.email }
       })
-      const groupArray=[]
-      for(var i=0;i<userGroups.data.groupByUserName.items.length;i++){
-        const groupDetails=await API.graphql({
-          query:queries.getGroup,
-          variables:{
-            id:userGroups.data.groupByUserName.items[i].groupID
+      const groupArray = []
+      for (var i = 0; i < userGroups.data.groupByUserName.items.length; i++) {
+        const groupDetails = await API.graphql({
+          query: queries.getGroup,
+          variables: {
+            id: userGroups.data.groupByUserName.items[i].groupID
           }
         })
         groupArray.push(groupDetails.data.getGroup)
@@ -81,18 +86,18 @@ const Groups=({navigation})=>{
   }
   return (
     <View>
-     <ImageBackground
+      {/* <ImageBackground
        style={GroupList.container}
        source={require('../Assets/background.png')} 
        resizeMode="cover"
-     >
+     > */}
       <FlatList
-              data={data}
-              renderItem={renderItem}
-              keyExtractor={item => item.GroupName}
-              numColumns={2}
-        />
-     </ImageBackground>
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.GroupName}
+        numColumns={2}
+      />
+      {/* </ImageBackground> */}
     </View>
   );
 };
--- src/Styles/GroupList.js
+++ src/Styles/GroupList.js
@@ -1,64 +1,76 @@
-import { StyleSheet ,Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-const GroupList=StyleSheet.create({
-    groupContaier:{
-        backgroundColor:"white",
-        width:windowWidth/2.3,
-        height:windowWidth/2.3,
-        elevation:5,
+const GroupList = StyleSheet.create({
+    groupContaier: {
+        backgroundColor: "white",
+        width: windowWidth / 2.3,
+        height: windowHeight / 5.5,
+        elevation: 5,
         margin: 10,
-        borderRadius:10,
-        shadowOffset:{
-            width:-10,
-            height:-10,
+        borderRadius: 10,
+        shadowOffset: {
+            width: -10,
+            height: -10,
         },
-        shadowOpacity:1,
-        shadowRadius:10,
-        shadowColor:"#1d1d1d",
-        borderWidth:2,
-        borderColor:"#A8A9A8"
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        shadowColor: "#1d1d1d",
+        borderWidth: 2,
+        borderColor: "#A8A9A8"
 
     },
-    grpName:{
-        color:"#2e2e2e",
-        fontWeight:"bold",
-        fontFamily:"Alias",
-        fontSize:25,
-        textAlign:"right",
-        paddingHorizontal:10,
-        width:"100%",
+    grpName: {
+        color: "#2e2e2e",
+        fontWeight: "bold",
+        fontFamily: "Alias",
+        fontSize: 20,
+        textAlign: "right",
+        paddingHorizontal: 10,
+        width: "100%",
+        textAlign: 'center'
     },
-    image:{
-        height:50,
-        width:50,
-        borderRadius:50/2,
-        marginLeft:15,
-        marginTop:15,
+    image: {
+        height: 50,
+        width: 50,
+        borderRadius: 50 / 2,
+        marginHorizontal: 50,
+        marginTop: 15,
+        // borderWidth: 2
+
     },
-    cardHeader:{
-        width:"100%",
-        height:'70%',
-        backgroundColor:"white",
-        borderTopRightRadius:10,
-        borderTopLeftRadius:10
+    cardHeader: {
+        width: "100%",
+        height: '70%',
+        backgroundColor: "white",
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        // borderWidth: 2
     },
-    cardFooter:{
-        width:'100%',
-        height:'30%',
-        backgroundColor:"#D9D9D9",
-        borderBottomRightRadius:10,
-        borderBottomLeftRadius:10,
-        borderTopWidth:1,
-        borderColor:"#A8A9A8"
+    crr: {
+        // borderWidth: 2,
+        justifyContent: 'center'
     },
-    footerTxt:{
-        color:"black",
-        fontSize:15,
-        padding:5
+    cardFooter: {
+        width: '100%',
+        height: '30%',
+        backgroundColor: "#D9D9D9",
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderTopWidth: 2,
+        borderColor: "#A8A9A8"
     },
-    container:{
-        alignItems:"center",
-    }
+    footerTxt: {
+        color: "black",
+        fontSize: 15,
+        padding: 5,
+        textAlign: 'center'
+        // flexDirection: 'row'
+        // , borderWidth: 1
+    },
+    container: {
+        alignItems: "center",
+    },
+
 })
 export default GroupList;
