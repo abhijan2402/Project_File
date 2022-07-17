10ef2c876c57bdff7ee359491531046ba4a18f80
abhijan2402 <abhishek.jangid643@gmail.com>
2022-07-17 11:38:03 +0530

    done chnaged in ui

--- src/Styles/GroupList.js
+++ src/Styles/GroupList.js
@@ -1,44 +1,46 @@
-import { StyleSheet ,Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-const GroupList=StyleSheet.create({
-    groupContainer:{
-        backgroundColor:"white",
-        width:windowWidth-20,
-        height:windowWidth/6,
-        elevation:5,
-        shadowOffset:{
-            width:-10,
-            height:-10,
+const GroupList = StyleSheet.create({
+    groupContainer: {
+        backgroundColor: "white",
+        width: windowWidth - 20,
+        height: windowWidth / 6,
+        elevation: 5,
+        shadowOffset: {
+            width: -20,
+            height: -20,
         },
-        shadowOpacity:1,
-        shadowRadius:10,
-        shadowColor:"#1d1d1d",
-        justifyContent:'center',
-        paddingHorizontal:5
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        shadowColor: "#1d1d1d",
+        justifyContent: 'center',
+        paddingHorizontal: 5,
+        marginVertical: 3,
+        borderRadius: 15
     },
-    grpName:{
-        color:"#2e2e2e",
-        fontWeight:"bold",
-        fontFamily:"Alias",
-        fontSize:25,
+    grpName: {
+        color: "#2e2e2e",
+        fontWeight: "bold",
+        fontFamily: "Alias",
+        fontSize: 25,
     },
-    image:{
-        height:50,
-        width:50,
-        borderRadius:50/2,
+    image: {
+        height: 50,
+        width: 50,
+        borderRadius: 50 / 2,
     },
-    cardHeader:{
-        height:'80%',
-        backgroundColor:"white",
+    cardHeader: {
+        height: '80%',
+        backgroundColor: "white",
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    container:{
-        alignItems:"center",
+    container: {
+        alignItems: "center",
     },
-    titleDesc:{
-        marginHorizontal:10
+    titleDesc: {
+        marginHorizontal: 10
     }
 })
 export default GroupList;
